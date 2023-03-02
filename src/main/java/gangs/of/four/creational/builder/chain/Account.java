8261e892eb92cd9421d2e10fa903a4d3138d0857
David <er.email>
2023-03-02 14:03:29 -0500

    little adjust in builder chain

--- src/main/java/gangs/of/four/creational/builder/chain/Account.java
+++ src/main/java/gangs/of/four/creational/builder/chain/Account.java
@@ -1,2 +1,51 @@
-package gangs.of.four.creational.builder.chain;public class Account {
+package gangs.of.four.creational.builder.chain;
+
+public class Account {
+
+    private String userId;
+    private String token;
+
+    private Account(){
+
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public static Builder newBuilder(){
+        return new Account().new Builder();
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "userId='" + userId + '\'' +
+                ", token='" + token + '\'' +
+                '}';
+    }
+
+    class Builder{
+
+        private Builder(){
+        }
+
+        public Builder setUserId(String userId){
+            Account.this.userId = userId;
+            return this;
+        }
+
+        public Builder setToken(String token){
+            Account.this.token = token;
+            return this;
+        }
+
+        public Account build(){
+            return Account.this;
+        }
+    }
 }
--- src/main/java/gangs/of/four/creational/builder/chain/Demo.java
+++ src/main/java/gangs/of/four/creational/builder/chain/Demo.java
@@ -1,2 +1,13 @@
-package gangs.of.four.creational.builder.chain;public class Demo {
+package gangs.of.four.creational.builder.chain;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Account account = Account.newBuilder()
+                .setToken("Hello")
+                .setUserId("habr")
+                .build();
+
+        System.out.println(account);
+    }
 }
